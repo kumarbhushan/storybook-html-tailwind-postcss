391e8f5a51b9788b536cb3d18054abeeeff7ad28
Bryan <bgold0@gmail.com>
2020-09-01 09:52:34 -0400

    Update -postcss.config.js

--- -postcss.config.js
+++ -postcss.config.js
@@ -1,27 +1,9 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
-// postcss.config.js
-const purgecss = require("@fullhuman/postcss-purgecss")({
-	whitelistPatterns: [/trix$/],
-  // Specify the paths to all of the template files in your project
-  content: [
-    // "./assets/**/*.html",
-    // "./assets/**/*.jsx",
-    // "./assets/**/*.tsx",
-    "./templates/**/*.tmpl",
-    "./templates/partials/*.tmpl",
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
 		tailwindcss,
 		autoprefixer,
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
